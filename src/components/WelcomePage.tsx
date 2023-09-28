dd0ae4371348c94eb8f29446c340b20d4e9ecc70
siva-sankar-007 <siva.sankar@thecloudside.com>
2023-09-28 14:48:50 +0530

    blue

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -275,7 +275,7 @@ function WelcomePage() {
                   }}
                 />
               </div>
-              <div className="discription" style={{border:'3px solid yellow'}}>
+              <div className="discription" style={{border:'3px solid yellow',backgroundColor:'blue'}}>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
                 deleniti minus ullam omnis optio autem voluptas porro quas
                 adipisci ratione explicabo aut pariatur cupiditate repellendus
